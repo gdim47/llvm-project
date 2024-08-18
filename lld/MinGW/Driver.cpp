a564c4189ca6c1a7629048d9d1be0f7c5aa74741
gdim47 <gdim47@protonmail.com>
2024-08-18 04:35:24 +0300

    hack: add support for ARM64X in MinGW toolchain

--- lld/MinGW/Driver.cpp
+++ lld/MinGW/Driver.cpp
@@ -359,6 +359,7 @@ bool link(ArrayRef<const char *> argsArr, llvm::raw_ostream &stdoutOS,
   if (args.getLastArgValue(OPT_m) != "thumb2pe" &&
       args.getLastArgValue(OPT_m) != "arm64pe" &&
       args.getLastArgValue(OPT_m) != "arm64ecpe" &&
+      args.getLastArgValue(OPT_m) != "arm64xpe" &&
       args.hasFlag(OPT_disable_dynamicbase, OPT_dynamicbase, false))
     add("-dynamicbase:no");
   if (args.hasFlag(OPT_disable_high_entropy_va, OPT_high_entropy_va, false))
@@ -424,6 +425,8 @@ bool link(ArrayRef<const char *> argsArr, llvm::raw_ostream &stdoutOS,
       add("-machine:arm64");
     else if (s == "arm64ecpe")
       add("-machine:arm64ec");
+    else if (s == "arm64xpe")
+      add("-machine:arm64x");
     else
       error("unknown parameter: -m" + s);
   }
